e5bc78374069a4749623e703fd86b92129440db1
sout_Nantang <55623894+sout233@users.noreply.github.com>
2022-12-15 21:06:19 +0800

    fix

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-window.location.href = "https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4";
+setTimeout(function() { jumpTo(); }, 2000);
+
+function jumpTo(){
+    window.location.href = "https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4";
+}
