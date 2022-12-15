7ef35f95e10d40e1d1ddcc3689b8c7fafb9c2e0f
sout_Nantang <55623894+sout233@users.noreply.github.com>
2022-12-15 21:25:23 +0800

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// setTimeout(function() { jumpTo(); }, 2000);
+setTimeout(function() { jumpTo(); }, 2000);
 
 function jumpTo(){
     window.location.href = "https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4";
